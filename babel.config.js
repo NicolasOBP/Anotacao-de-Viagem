abca74b552ecc0786ddfcbb8ed13c739bd93634d
NicolasOBP <nicolasobp15@gmail.com>
2024-07-30 14:53:05 -0300

    fix: absolute path

--- babel.config.js
+++ babel.config.js
@@ -2,27 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      "react-native-reanimated/plugin",
-      [
-        "module-resolver",
-        {
-          root: ".",
-          extensions: [
-            ".js",
-            ".jsx",
-            ".ts",
-            ".tsx",
-            ".android.js",
-            ".android.tsx",
-            ".ios.js",
-            ".ios.tsx",
-          ],
-          alias: {
-            "@": "./src",
-          },
-        },
-      ],
-    ],
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
